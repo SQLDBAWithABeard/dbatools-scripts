424038b4f7a7dbdb0b1547347e22acf04ce8b0d2
Rob <Rob@TheBeard.local>
2017-04-14 07:10:45 +0100

    Added comments

--- Pester Test Identity Usage  - Server Level.ps1
+++ Pester Test Identity Usage  - Server Level.ps1
@@ -1,12 +1,11 @@
+Describe "Testing how full the Identity columns are" {
+## This is getting a list of server name from Hyper-V - You can chagne this to a list of SQL instances
+$SQLServers = (Get-VM -ComputerName beardnuc | Where-Object {$_.Name -like '*SQL*' -and $_.Name -ne 'SQL2008Ser2008' -and $_.State -eq 'Running'}).Name
 
-Describe "Testing Identity Usage" {
-    $SQLServers = (Get-VM -ComputerName HYPERVServer | Where-Object {$_.Name -like '*SQL*' -and $_.State -eq 'Running'}).Name
-    $testCases= @()
-    $SQLServers.ForEach{$testCases += @{Name = $_}}
-    It "<Name> databases have all had a successful CheckDB within the last 7 days" -TestCases $testCases {
-        Param($Name)
-        $DBCC = Get-DbaLastGoodCheckDb -SqlServer $Name -Detailed
-        $DBCC.Status -contains 'New database, not checked yet'| Should Be $false
-        $DBCC.Status -contains 'CheckDb should be performed'| Should Be $false
-    }
+$testCases= @()
+$SQLServers.ForEach{$testCases += @{Name = $_}}
+It "<Name> databases all have identity columns less than 90% full" -TestCases $testCases {
+Param($Name)
+(Test-DbaIdentityUsage -SqlInstance $Name -Threshold 90 -WarningAction SilentlyContinue).PercentUsed | Should Be
+}
 }
